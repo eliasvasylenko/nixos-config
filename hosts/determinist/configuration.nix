2128ee9d3c32ec6f128318b3799849a9efbe9f96
Elias Vasylenko <elias@vasylenko.uk>
2025-01-10 21:22:46 +0000

    add ssh

--- hosts/determinist/configuration.nix
+++ hosts/determinist/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
 
 {
@@ -15,6 +11,7 @@
 
     ../../services/caddy.nix
     ../../services/forgejo.nix
+    ../../services/ssh.nix
   ];
 
   boot.swraid.enable = true;
